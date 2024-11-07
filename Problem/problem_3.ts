8c2901091d10f6ede1d90d8e950ca8d866fb4553
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-11-07 21:04:53 +0600

    complited third problem

---
+++ Problem/problem_3.ts
@@ -0,0 +1,18 @@
+{
+  //
+  function countWordOccurrences(sentence: string, word: string) {
+    const lowerCaseSentence = sentence.toLowerCase();
+    const lowerCaseWord = word.toLowerCase();
+    const sentenceArry = lowerCaseSentence.split(" ");
+    const appearsNumber = sentenceArry.filter(
+      (item) => item === lowerCaseWord
+    ).length;
+    return appearsNumber;
+  }
+//   console.log(countWordOccurrences("count COunt COUNT count","count"));
+  
+
+
+
+  //
+}
