a7c08a57a5d86e0de07ee5dd373383b768ef3712
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-11-07 15:57:02 +0600

    finished first problem

---
+++ Problem/problem_1.ts
@@ -0,0 +1,12 @@
+{
+  //
+  function sumArray(nums: number[]): number {
+    const sumNumber = nums.reduce((acc, c) => acc + c, 0);
+    return sumNumber;
+  }
+
+//   const res = sumArray([55,55])
+  
+
+  //
+}
