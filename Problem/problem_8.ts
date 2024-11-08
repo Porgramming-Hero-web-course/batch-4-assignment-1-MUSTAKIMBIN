b86f33ac40c242fd697f475604368f0c0a64156e
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-11-08 14:26:32 +0600

    complited eighted problem

---
+++ Problem/problem_8.ts
@@ -0,0 +1,11 @@
+{
+  //
+  function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+    return keys.every((key) => key in obj);
+  }
+
+  const person = { name: "mustakim", age: 20, email: "practice@go.com" };
+  console.log(validateKeys(person, ["name", "age"]));
+
+  //
+}
